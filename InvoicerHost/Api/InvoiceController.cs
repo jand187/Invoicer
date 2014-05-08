5c28bb425b53182ea0dcd49fc94973b62470a3d2
Jan Daniel Andersen <jand187@gmail.com>
2014-05-08 11:56:57 +0200

    Added InvoiceController.

---
+++ InvoicerHost/Api/InvoiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace InvoicerHost.Api
+{
+	public class InvoiceController : ApiController
+	{
+		public void Post(Invoice invoice)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class Invoice
+	{
+		public DateTime PeriodStart { get; set; }
+		public DateTime PeriodEnd { get; set; }
+		public decimal Hours { get; set; }
+		public int InvoiceNumber { get; set; }
+	}
+}
