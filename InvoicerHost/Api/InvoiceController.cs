2ba67e1d80d879dd1554c4b3fe9a924b18cda72d
Jan Daniel Andersen <jand187@gmail.com>
2014-05-08 16:49:27 +0200

    Added pechkin.

--- InvoicerHost/Api/InvoiceController.cs
+++ InvoicerHost/Api/InvoiceController.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
 using System.Web.Http;
+using Pechkin;
 
 namespace InvoicerHost.Api
 {
 	public class InvoiceController : ApiController
 	{
+		public string Get()
+		{
+			var config = new GlobalConfig();
+			config.SetMargins(0, 0, 100, 0);
+			config.SetOutputDpi(300);
+			config.SetPaperSize(PaperKind.A4);
+
+			var converter = Factory.Create(config);
+			var contents = File.ReadAllText(@"C:\Users\janand\Documents\GitHub\Invoicer\InvoicerHost\invoice.html");
+			var pdfBytes = converter.Convert(contents);
+
+			using (var writer = new BinaryWriter(new FileStream(@"C:\Users\janand\Documents\test.pdf", FileMode.Create)))
+			{
+				writer.Write(pdfBytes);
+			}
+
+			return "yeah!";
+		}
+
 		public void Post(Invoice invoice)
 		{
 			throw new NotImplementedException();
